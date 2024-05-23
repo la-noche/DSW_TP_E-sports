7ffd14558ca0ba28d836448e7241f272636d0def
Leo Carovana <leonelcarovana11@gmail.com>
2024-05-23 13:36:35 -0300

    servidor lanzado y creada la carpeta dist

--- tp/src/app.ts
+++ tp/src/app.ts
@@ -8,6 +8,7 @@ app.use((_, res) => {
   return res.status(404).send({ message: 'Resource not found' })
 })
 
+
 app.listen(3000, () => {
   console.log('Server runnning on http://localhost:3000/')
 })
